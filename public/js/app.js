18ca5d2c71ed9229098fd40833d539c87ba73717
8thDawn <agevanz52@gmail.com>
2021-10-31 13:33:34 +0000

    Second commit video background

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,10 @@
 console.log(
     'Welcome to JNard 3d rendering services!!');
+
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    showcase.classList.toggle('active');
+    })
